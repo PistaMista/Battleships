a3b837edcc47a9c6be23196f7ab1e34ad802d4ba
PistaMista <pistamista@centrum.cz>
2017-03-30 22:20:49 +0200

    The fleet now gets positioned correctly in the action shot.

--- Assets/Scripts/Battle/BattleInterface.cs
+++ Assets/Scripts/Battle/BattleInterface.cs
@@ -248,6 +248,12 @@ public class BattleInterface : MonoBehaviour
             case BattleState.SHOWING_HIT_TILE:
                 Destroy(recentlyShotTileIndicator);
                 break;
+            case BattleState.FIRING:
+                foreach (Ship ship in battle.attackingPlayer.livingShips)
+                {
+                    ship.PositionOnPlayingBoard();
+                }
+                break;
         }
 
         switch (switchingTo)
@@ -331,21 +337,21 @@ public class BattleInterface : MonoBehaviour
 
         if (Mathf.Abs(battle.defendingPlayer.board.position.z) < Mathf.Abs(battle.defendingPlayer.board.position.x))
         {
-            fleetPosition = battle.defendingPlayer.board.position - Vector3.right * GameController.playerBoardDistanceFromCenter * Mathf.Sign(battle.defendingPlayer.board.position.x);
+            fleetPosition = battle.defendingPlayer.board.position - Vector3.right * GameController.playerBoardDistanceFromCenter * Mathf.Sign(battle.defendingPlayer.board.position.x) * 1.5f;
             fleetRotation = 90f * Mathf.Sign(battle.defendingPlayer.board.position.x);
         }
         else
         {
-            fleetPosition = battle.defendingPlayer.board.position - Vector3.forward * GameController.playerBoardDistanceFromCenter * Mathf.Sign(battle.defendingPlayer.board.position.z);
+            fleetPosition = battle.defendingPlayer.board.position - Vector3.forward * GameController.playerBoardDistanceFromCenter * Mathf.Sign(battle.defendingPlayer.board.position.z) * 1.5f;
             fleetRotation = 90f - 90f * Mathf.Sign(battle.defendingPlayer.board.position.z);
         }
 
+        fleetPosition.y = GameController.seaLevel;
 
-
-        GameObject tmp = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        tmp.transform.position = fleetPosition;
-        tmp.transform.localScale = new Vector3(0.1f, 0.1f, 2f);
-        tmp.transform.localRotation = Quaternion.Euler(new Vector3(0f, fleetRotation, 0f));
+        // GameObject tmp = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        // tmp.transform.position = fleetPosition;
+        // tmp.transform.localScale = new Vector3(0.1f, 0.1f, 2f);
+        // tmp.transform.localRotation = Quaternion.Euler(new Vector3(0f, fleetRotation, 0f));
 
         List<Ship> destroyers = new List<Ship>();
         List<Ship> cruisers = new List<Ship>();
@@ -366,12 +372,49 @@ public class BattleInterface : MonoBehaviour
             }
         }
 
+        Vector3 position = Vector3.zero;
+        //Place the battleships
+        for (int i = 0; i < battleships.Count; i++)
+        {
+            Ship ship = battleships[i];
+            position = new Vector3(-(battleships.Count / 2f - 0.5f) * 4f + (i) * 4f, ship.transform.position.y, 0f);
+            ship.transform.position = position;
+        }
 
+        //Place the destroyers
+        for (int i = 0; i < destroyers.Count; i++)
+        {
+            Ship ship = destroyers[i];
+            position = new Vector3(-(destroyers.Count / 2f - 0.5f) * 3f + (i) * 3f, ship.transform.position.y, 5.5f);
+            ship.transform.position = position;
+        }
+
+        //Place the cruisers
+        for (int i = 0; i < cruisers.Count; i++)
+        {
+            Ship ship = cruisers[i];
+            position = new Vector3(-(cruisers.Count / 2f - 0.5f) * 5.5f + (i) * 5.5f, ship.transform.position.y, -5.5f);
+            ship.transform.position = position;
+        }
+
+        //Rotate the ships
+        //Vector3 directionModifier = new Vector3(Mathf.Cos(fleetRotation * Mathf.Deg2Rad), 0f, Mathf.Sin(fleetRotation * Mathf.Deg2Rad)).normalized;
+
+        foreach (Ship ship in battle.attackingPlayer.livingShips)
+        {
+            ship.gameObject.SetActive(true);
+            ship.transform.rotation = Quaternion.Euler(Vector3.up * fleetRotation);
+            Vector3 currentDirection = ship.transform.position.normalized;
+            float radianAngle = fleetRotation * Mathf.Deg2Rad;
+            //Vector3 targetDirection = new Vector3(Mathf.Cos(fleetRotation * Mathf.Deg2Rad), 0f, Mathf.Sin(fleetRotation * Mathf.Deg2Rad)).normalized;
+            Vector3 localPosition = new Vector3(ship.transform.position.x * Mathf.Cos(radianAngle) - ship.transform.position.z * Mathf.Sin(radianAngle), 0f, ship.transform.position.z * Mathf.Cos(radianAngle) + ship.transform.position.x * Mathf.Sin(radianAngle));
+            ship.transform.position = localPosition + fleetPosition;
+        }
 
     }
 
     /// <summary>
-    /// Cycles the action shot.
+    /// Refreshes the action shot.
     /// </summary>
     static void RefreshActionView()
     {
